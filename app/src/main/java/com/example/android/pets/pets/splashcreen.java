599165ac2e4f6d9a506e4f0ad0723a5513a9fa31
sam7244 <shamithkumar7244@gmail.com>
2022-01-07 11:48:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/pets/pets/splashcreen.java
@@ -0,0 +1,14 @@
+package com.example.android.pets.pets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashcreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashcreen);
+    }
+}
